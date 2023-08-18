2ee894b0b820f22cbd93a6eb9bb1beaff4ebee9c
unknown <agy.calo90@gmail.com>
2023-08-18 17:58:33 -0300

    creacion html,css varios y base de datos json. falta instalar json server y demas

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,15 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  //constructor(private readonly appService: AppService) {}
+//
+  //@Get()
+  //getHello(): string {
+  //  return this.appService.getHello();
+  //}
 }
+
+
+//consultar porqeu debo borrar toda la clase jutno con sus metodos en 
+//app.controller y app.servis para que m,e muestre mi html
+// pero los demas no borran nada! 🤬
